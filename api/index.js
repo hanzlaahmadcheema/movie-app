00f8968d41a164de8c00ae759e5d61d725afb8d2
h-a-entertainment <wasilahmad100@gmail.com>
2024-08-01 22:50:27 +0500

    l

--- api/index.js
+++ api/index.js
@@ -86,30 +86,46 @@ app.get('/', async (req, res) => {
 });
 
 // Define the search route
+// Define the search route
 app.get('/search', async (req, res) => {
-  const keyword = req.query.keyword || '';
-  const page = parseInt(req.query.page, 10) || 1;
+    const keyword = req.query.keyword || '';
+    const page = parseInt(req.query.page, 10) || 1;
+    
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${encodeURIComponent(keyword)}&page=${page}`);
+        const data = await response.json();
   
-  try {
-      const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${encodeURIComponent(keyword)}&page=${page}`);
-      const data = await response.json();
-
-      const combinedResults = data.results;
-      const totalPages = data.total_pages;
-      const currentPage = data.page;
-
-      res.render('search', {
-          searchName: `Search results for "${keyword}"`,
-          combinedResults,
-          totalPages,
-          currentPage,
-          keyword // Pass the keyword to the template
-      });
-  } catch (error) {
-      console.error('Error fetching search results:', error);
-      res.status(500).send('An error occurred while searching.');
-  }
-});
+        const combinedResults = data.results;
+        const totalPages = data.total_pages;
+        const currentPage = data.page;
+  
+        // Default image
+        let imageUrl = 'https://ha-entertainment.vercel.app/images/logo.png';
+  
+        // Check if there are results and use the image of the first result
+        if (combinedResults.length > 0) {
+            const firstResult = combinedResults[0];
+            if (firstResult.poster_path) {
+                imageUrl = `https://image.tmdb.org/t/p/original${firstResult.backdrop_path}`;
+            } else if (firstResult.backdrop_path) {
+                imageUrl = `https://image.tmdb.org/t/p/original${firstResult.poster_path}`;
+            }
+        }
+  
+        res.render('search', {
+            searchName: keyword.replace(/\+/g, ' '),  // Replace '+' with space for display
+            combinedResults,
+            totalPages,
+            currentPage,
+            keyword,
+            imageUrl
+        });
+    } catch (error) {
+        console.error('Error fetching search results:', error);
+        res.status(500).send('An error occurred while searching.');
+    }
+  });
+  
 
 app.get('/search/suggest', async (req, res) => {
   const keyword = req.query.keyword || '';
@@ -271,6 +287,7 @@ app.get('/movie/:id', async (req, res) => {
       const movie = moviesResponse.data;
       const relatedMovies = moviesrelatedResponse.data.results;
       const cast = moviescreditsResponse.data.cast;
+      
 
       // Attach cast information to the movie object
       movie.cast = cast.slice(0, 7); // Limit to top 5 cast members
@@ -290,87 +307,166 @@ app.get('/movie/:id', async (req, res) => {
 });
 
 app.get('/series/:id', async (req, res) => {
-  const tvId = req.params.id;
-  try {
-      // Fetch TV series details, related TV series, and cast information
-      const [tvResponse, tvRelatedResponse, tvCreditsResponse] = await Promise.all([
-          axios.get(`https://api.themoviedb.org/3/tv/${tvId}?api_key=${apiKey}&language=en-US`),
-          axios.get(`https://api.themoviedb.org/3/tv/${tvId}/recommendations?api_key=${apiKey}&language=en-US`),
-          axios.get(`https://api.themoviedb.org/3/tv/${tvId}/credits?api_key=${apiKey}&language=en-US`)
-      ]);
-
-      const tv = tvResponse.data;
-      const relatedTv = tvRelatedResponse.data.results;
-      const cast = tvCreditsResponse.data.cast;
-
-      tv.cast = cast.slice(0, 5); // Limit to top 5 cast members
-
-      // Define getRatingClass function
-      const getRatingClass = (rating) => {
-          const starRating = Math.round(rating / 2);
-          return `fill-${starRating}`;
-      };
-
-
-      res.render('series', { tv, relatedTv, getRatingClass });
-  } catch (error) {
-      console.error('Error fetching TV data:', error);
-      res.status(500).send('Error fetching TV data');
-  }
-});
+    const tvId = req.params.id;
+    try {
+        // Fetch TV series details, related TV series, and cast information
+        const [tvResponse, tvRelatedResponse, tvCreditsResponse, tvVideosResponse] = await Promise.all([
+            axios.get(`https://api.themoviedb.org/3/tv/${tvId}?api_key=${apiKey}&language=en-US`),
+            axios.get(`https://api.themoviedb.org/3/tv/${tvId}/recommendations?api_key=${apiKey}&language=en-US`),
+            axios.get(`https://api.themoviedb.org/3/tv/${tvId}/credits?api_key=${apiKey}&language=en-US`),
+            axios.get(`https://api.themoviedb.org/3/tv/${tvId}/videos?api_key=${apiKey}&language=en-US`)
+        ]);
+  
+        const tv = tvResponse.data;
+        const relatedTv = tvRelatedResponse.data.results;
+        const cast = tvCreditsResponse.data.cast;
+        const videos = tvVideosResponse.data.results;
+  
+        const trailerId = videos.find(video => video.type === 'Trailer' && video.site === 'YouTube')?.key || null;
+  
+        tv.cast = cast.slice(0, 5); // Limit to top 5 cast members
+  
+        // Define getRatingClass function
+        const getRatingClass = (rating) => {
+            const starRating = Math.round(rating / 2);
+            return `fill-${starRating}`;
+        };
+  
+        res.render('series', { tv, relatedTv, getRatingClass, trailerId, cast });
+    } catch (error) {
+        console.error('Error fetching TV data:', error);
+        res.status(500).send('Error fetching TV data');
+    }
+  });  
 
 app.get('/watch-movie/:id', async (req, res) => {
-  try {
+    try {
       const movieId = req.params.id;
       const movieDetails = await getMovieDetails(movieId);
+      const trailerId = await getTrailerId(movieId); // Function to fetch trailer ID
+      const castDetails = await getCastDetails(movieId); // Function to fetch cast details
       
-      // Render the view with movie details, servers, and related movies
       res.render('watch-movie', {
-          movie: movieDetails.movie,
-          servers: movieDetails.servers,
-          relatedMovies: movieDetails.relatedMovies
+        movie: movieDetails.movie,
+        servers: movieDetails.servers,
+        relatedMovies: movieDetails.relatedMovies,
+        trailerId: trailerId,  // Pass trailer ID to the template
+        cast: castDetails      // Pass cast details to the template
       });
-  } catch (error) {
+    } catch (error) {
       console.error('Error fetching movie details:', error);
       res.status(500).send('Internal Server Error');
-  }
-});
-
-// Function to fetch movie details from the TMDb API
-async function getMovieDetails(movieId) {
-  try {
-      // Fetch movie details from TMDb API
+    }
+  });
+  
+  // Function to fetch movie details from the TMDb API
+  async function getMovieDetails(movieId) {
+    try {
       const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`);
       const movie = response.data;
-
-      // Fetch related movies from TMDb API
+  
       const recommendationsResponse = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${apiKey}&language=en-US`);
       const relatedMovies = recommendationsResponse.data.results;
-
-      // Map the fetched movie data to your template's expected structure
+  
       const movieDetails = {
-          id: movie.id,
-          title: movie.title,
-          overview: movie.overview,
-          backdropPath: movie.backdrop_path,
-          posterPath: movie.poster_path,
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdropPath: movie.backdrop_path,
+        posterPath: movie.poster_path,
+        genres: movie.genres,
+        release_date: movie.release_date,
+        // Add any other movie details you need
       };
-
-      // Define your servers
+  
       const servers = [
-          { id: '1', name: 'H.A', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${movieId}` },
-          { id: '2', name: 'UpCloud', url: `https://vidsrc.xyz/embed/movie?tmdb=${movieId}` },
-          { id: '3', name: 'MegaCloud', url: `https://www.2embed.cc/embed/${movieId}` },
-          { id: '4', name: 'MovieUniverse', url: `https://movieuniverse.lol/embed/${movieId}` }
+        { id: '1', name: 'H.A', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${movieId}` },
+        { id: '2', name: 'UpCloud', url: `https://vidsrc.xyz/embed/movie?tmdb=${movieId}` },
+        { id: '3', name: 'MegaCloud', url: `https://www.2embed.cc/embed/${movieId}` },
+        { id: '4', name: 'MovieUniverse', url: `https://movieuniverse.lol/embed/${movieId}` }
       ];
-
+  
       return { movie: movieDetails, servers: servers, relatedMovies: relatedMovies };
-
-  } catch (error) {
+    } catch (error) {
       console.error('Error fetching movie details from TMDb:', error);
       throw new Error('Failed to fetch movie details');
+    }
   }
-}
+  
+  // Function to fetch trailer ID
+  async function getTrailerId(movieId) {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`);
+      const videos = response.data.results;
+      
+      const trailer = videos.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+      return trailer ? trailer.key : null;
+    } catch (error) {
+      console.error('Error fetching trailer ID:', error);
+      return null;
+    }
+  }
+  
+  // Function to fetch cast details
+  async function getCastDetails(movieId) {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=en-US`);
+      const cast = response.data.cast; // Get the cast details
+  
+      // Map cast details to include required fields
+      const castDetails = cast.map(person => ({
+        id: person.id,
+        name: person.name,
+        character: person.character,
+        profilePath: person.profile_path
+      }));
+  
+      return castDetails;
+    } catch (error) {
+      console.error('Error fetching cast details:', error);
+      return [];
+    }
+  } 
+
+  app.get('/watch-series/:id/season/:season/episode/:episode', async (req, res) => {
+    const { id, season, episode } = req.params;
+
+    try {
+        // Fetch series details
+        const seriesResponse = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`);
+        const series = seriesResponse.data;
+
+        // Fetch all seasons
+        const seasons = series.seasons;
+
+        // Fetch episodes for the selected season
+        const episodesResponse = await axios.get(`https://api.themoviedb.org/3/tv/${id}/season/${season}?api_key=${apiKey}`);
+        const episodes = episodesResponse.data.episodes;
+
+        // Get the current episode ID or the first episode in the season by default
+        const currentEpisode = episodes.find(ep => ep.episode_number == episode) || episodes[0];
+
+        // Servers data with URLs
+        const servers = [
+            { name: 'HA Entertainment', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${currentEpisode.id}` },
+            { name: 'VidSrc', url: `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${season}&episode=${currentEpisode.episode_number}` }
+        ];
+
+        // Render the EJS template with series, seasons, episodes, and servers data
+        res.render('watch-series', {
+            series,
+            seasons,
+            episodes,
+            selectedSeason: season,
+            currentEpisode,
+            servers
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching series data');
+    }
+});
+
 
 // Route to capture cast details
 app.get('/cast/:id', async (req, res) => {
@@ -580,10 +676,12 @@ app.get('/production/:id', async (req, res) => {
           params: { api_key: apiKey }
       });
       const productionName = productionResponse.data.name;
+      const Logo = productionResponse.data.logo_path;
 
       res.render('production', {
           productionId,
           productionName,
+          Logo,
           combinedResults,
           totalPages,
           currentPage: parseInt(req.query.page) || 1
@@ -647,45 +745,18 @@ app.get('/filter', async (req, res) => {
   }
 });
 
-app.get('/watch-series/:id', async (req, res) => {
-  const { id } = req.params;
-  const { season = 1, episode = 1 } = req.query; // Default values
-
-  try {
-      // Fetch series details
-      const seriesResponse = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`);
-      const series = seriesResponse.data;
-
-      // Fetch all seasons
-      const seasons = series.seasons;
-
-      // Fetch episodes for the selected season
-      const episodesResponse = await axios.get(`https://api.themoviedb.org/3/tv/${id}/season/${season}?api_key=${apiKey}`);
-      const episodes = episodesResponse.data.episodes;
-
-      // Get the current episode ID or the first episode in the season by default
-      const currentEpisode = episodes.find(ep => ep.episode_number == episode) || episodes[0];
-
-      // Servers data with URLs
-      const servers = [
-          { name: 'HA Entertainment', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${currentEpisode.id}` },
-          { name: 'VidSrc', url: `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${season}&episode=${currentEpisode.episode_number}` }
-      ];
-
-      // Render the EJS template with series, seasons, episodes, and servers data
-      res.render('watch-series', {
-          series,
-          seasons,
-          episodes,
-          selectedSeason: season,
-          currentEpisode,
-          servers
-      });
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Error fetching series data');
-  }
+app.get('/watch-series/:id', (req, res) => {
+    const { id } = req.params;
+    res.redirect(`/watch-series/${id}/season/1/episode/1`);
 });
+
+app.get('/watch-series/:id/season/:season', (req, res) => {
+    const { id, season } = req.params;
+    res.redirect(`/watch-series/${id}/season/${season}/episode/1`);
+});
+
+
+
 
 
 module.exports = app;
