726536f6bb93ebfe8b4e390973a604fb1ac1e213
h-a-entertainment <wasilahmad100@gmail.com>
2024-08-06 20:46:37 +0500

    j

--- api/index.js
+++ api/index.js
@@ -427,8 +427,8 @@ async function getMovieDetails(movieId) {
             { id: '1', name: 'English', url: iframeUrl },
             { id: '2', name: 'Urdu/Hindi', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${movieId}` },
             { id: '3', name: 'Urdu/Hindi', url: `https://ha-entertainment.netlify.app/embed/watch-embed2?id=${movieId}` },
-            { id: '4', name: 'English', url: `https://multiembed.mov/?video_id=${movieId}&tmdb=1` },
-            { id: '5', name: 'English', url: `https://moviesapi.club/movie/${movieId}` },
+            { id: '4', name: 'English', url: `https://moviesapi.club/movie/${movieId}` },
+            { id: '5', name: 'English', url: `https://multiembed.mov/?video_id=${movieId}&tmdb=1` },
             { id: '6', name: 'English', url: `https://vidsrc.xyz/embed/movie?tmdb=${movieId}` },
             { id: '7', name: 'English', url: `https://www.2embed.cc/embed/${movieId}` },
         ];
@@ -495,10 +495,10 @@ async function getMovieDetails(movieId) {
 
         // Servers data with URLs
         const servers = [
-            { name: 'English', url: `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${season}&e=${currentEpisode.episode_number}` },
+            { name: 'English', url: `https://moviesapi.club/tv/${id}-${season}-${currentEpisode.episode_number}` },
             { name: 'Hindi/Urdu', url: `https://ha-entertainment.netlify.app/embed/watch-embed?id=${currentEpisode.id}` },
             { name: 'Hindi/Urdu', url: `https://ha-entertainment.netlify.app/embed/watch-embed2?id=${currentEpisode.id}` },
-            { name: 'English', url: `https://moviesapi.club/tv/${id}-${season}-${currentEpisode.episode_number}` },
+            { name: 'English', url: `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${season}&e=${currentEpisode.episode_number}` },
             { name: 'English', url: `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${season}&episode=${currentEpisode.episode_number}` },
             { name: 'English', url: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${currentEpisode.episode_number}` },
             { name: 'English', url: `https://movieuniverse.lol/embedtv/${id}&season=${season}&episode=${currentEpisode.episode_number}` }
