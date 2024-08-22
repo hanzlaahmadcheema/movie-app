3d15614585873cf5359c35f229ecbb7300ef4a2f
h-a-entertainment <wasilahmad100@gmail.com>
2024-08-22 13:48:00 +0500

    l

--- api/index.js
+++ api/index.js
@@ -880,10 +880,6 @@ app.get('/sitemap.xml', async (req, res) => {
 app.get('/sitemap-page.xml', (req, res) => {
     const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-            xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" 
-            xmlns:xhtml="http://www.w3.org/1999/xhtml" 
-            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
-            xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
         <url>
             <loc>https://ha-entertainment.com/</loc>
             <changefreq>daily</changefreq>
